85290832e430ec5f2dd924de834bd6a756f36a46
Mike Vlah <vlahm13@gmail.com>
2020-06-07 14:16:25 -0400

    saving ancient changes before starting tf tutorial

--- simple_input_testing.py
+++ simple_input_testing.py
@@ -1,8 +1,4 @@
-from Queue import Queue
-from threading import *
-from select import select
 import sys
-import thread
 import tty, termios
 
 class _GetchUnix:
@@ -34,18 +30,14 @@ getch = _GetchUnix()
        #     termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
       # q.put(i)
 
-def chili(q):
-    while True:
-        print getch()
+#def chili(q):
+#    while True:
+#        print getch()
+print(getch())
        # x = q.get()
       #  print 'you said' + sys.stdin.readline() 
        # q.task_done()
 
-q = Queue(maxsize=0)
-#thread1 = Thread(target=fart, args=(q,))
-thread2 = Thread(target=chili, args=(q,))
-#thread1.start()
-thread2.start()
 
 #print 'go'
 #i,o,e = select([sys.stdin], [], [], 5)
